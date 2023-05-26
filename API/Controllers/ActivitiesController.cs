21a85d07f477684ebce2bead4193e3be08042223
Jay Mangune <achaeleus@gmail.com>
2023-05-26 21:01:19 +0800

    First Commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public ActivitiesController(DataContext dataContext )
+        { 
+            context = dataContext;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> getActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> getActivity(Guid id)
+        {
+            return await context.Activities.FindAsync(id);
+        }
+
+
+    }
+
+}
---
+++ Persistence/Migrations/20230525141245_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<int>(type: "nvarchar(max)", nullable: false),
+                    Venue = table.Column<int>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Activities");
+        }
+    }
+}
